2eb5809afda6302557776ca24bc24e1959a9814f
Jose Javier Bailón Ortiz <josebailon@gmail.com>
2023-10-04 13:28:11 +0200

    ajuste final

--- src/reto2/HebraReloj.java
+++ src/reto2/HebraReloj.java
@@ -4,38 +4,37 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JLabel;
 
-
 /**
  * Hebra que controla la actualizacion de la etiqueta de la hora actual
  */
 public class HebraReloj extends Thread {
-	
+
 	/**
 	 * Referencia a la etiqueta donde se muestra la hora
 	 */
 	JLabel visorReloj;
-	
-	
+
 	/**
 	 * Constructor
 	 * 
 	 * @param visorReloj Etiqueta en la que se muestra la hora
 	 */
 	public HebraReloj(JLabel visorReloj) {
-		this.visorReloj=visorReloj;
+		this.visorReloj = visorReloj;
 	}
 
 	@Override
 	public void run() {
+		//Actualizar etiqueta del reloj cada 200ms 
 		SimpleDateFormat formato = new SimpleDateFormat("HH:mm:ss");
-		while(true) {
-		Date fechaActual = new Date();
-		visorReloj.setText("Son las "+formato.format(fechaActual));
-		try {
-			sleep(200);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		while (true) {
+			Date fechaActual = new Date();
+			visorReloj.setText("Son las " + formato.format(fechaActual));
+			try {
+				sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
